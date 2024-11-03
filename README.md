d9c0d40f8198bd3326ff984f2d057d9e21ce69ed
igromanru <igromanru@yahoo.de>
2024-11-03 13:09:39 +0100

    feat: Add set_outline_color function to fix outline color docs: Add All Enemies preview

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Always Outline mod for Warhammer 40,000: Darktide
-The mod applies outlines to a all or a chosen enemy group automatically.
+The mod applies outlines to all or chosen enemy groups automatically.
 
+## Settings
+Per default the mod uses the color [Grey](https://www.colorhexa.com/808080) (128, 128, 128) for all outlines. If you want to improve the visibility, you can simply change it to [White](https://www.colorhexa.com/ffffff) (255, 255, 255).  
+Per default only Specialist, Elite and Monstrosity are enabled.  
+ 
 ## All Enemies
 The "All Enemies" option will outline all possible enemy types.  
 But it doesn't work for normal horde enemies like Poxwolker or Groaner, since the game has no outline data for their models. 
+![All](./info/All_Enemies.jpg)
 
 ## Specialist
 Outlines [specialists](https://darktide.gameslantern.com/enemies?type=SPECIALIST).
--- scripts/mods/AlwaysOutline/AlwaysOutline.lua
+++ scripts/mods/AlwaysOutline/AlwaysOutline.lua
@@ -2,11 +2,16 @@ local mod = get_mod("AlwaysOutline")
 
 local OutlineSettings = require("scripts/settings/outline/outline_settings")
 
+local ALL_ENEMY = "all_enemy"
+local SPECIALIST_ENEMY = "specialist_enemy"
+local ELITE_ENEMY = "elite_enemy"
+local MONSTER_ENEMY = "monster_enemy"
+
 local ENABLE_MOD_SETTING = "enable_mod"
-local ALL_ENEMY_ENABLED_SETTING = "all_enemy_enabled"
-local SPECIALIST_ENEMY_SETTING = "specialist_enemy_enabled"
-local ELITE_ENEMY_SETTING = "elite_enemy_enabled"
-local MONSTER_ENEMY_ENABLED_SETTING = "monster_enemy_enabled"
+local ALL_ENEMY_ENABLED_SETTING = ALL_ENEMY .. "_enabled"
+local SPECIALIST_ENEMY_SETTING = SPECIALIST_ENEMY .. "_enabled"
+local ELITE_ENEMY_SETTING = ELITE_ENEMY .. "_enabled"
+local MONSTER_ENEMY_ENABLED_SETTING = MONSTER_ENEMY .. "_enabled"
 
 local OUTLINE_MATERIAL_LAYERS = {
 	"minion_outline",
@@ -18,14 +23,6 @@ local function round(num, decimal_places)
     return math.floor(num * mult + 0.5) / mult
 end
 
-local get_color_array = function(name)
-	local r = round(tonumber(mod:get(name.."_r")) / 255, 3)
-	local g = round(tonumber(mod:get(name.."_g")) / 255, 3)
-	local b = round(tonumber(mod:get(name.."_b")) / 255, 3)
-
-	return {r,g,b}
-end
-
 local function visibility_check(unit)
 	if not unit or not HEALTH_ALIVE[unit] then
 		return false
@@ -33,39 +30,47 @@ local function visibility_check(unit)
 	return true
 end
 
+mod.get_color_array = function(self, name)
+	local r = round(tonumber(self:get(name.."_r")) / 255, 3)
+	local g = round(tonumber(self:get(name.."_g")) / 255, 3)
+	local b = round(tonumber(self:get(name.."_b")) / 255, 3)
+
+	return {r,g,b}
+end
+
 mod.update_outline_settings = function(self, instance)
 	if not instance then return end
 
-	instance.MinionOutlineExtension.all_enemy = {
-		priority = mod:get("all_enemy_priority"),
+	instance.MinionOutlineExtension[ALL_ENEMY] = {
+		priority = self:get(ALL_ENEMY .. "_priority"),
+		color = self:get_color_array(ALL_ENEMY),
 		material_layers = OUTLINE_MATERIAL_LAYERS,
-		color = get_color_array("all_enemy"),
 		visibility_check = visibility_check
 	}
 
-	instance.MinionOutlineExtension.specialist_enemy = {
-		priority = mod:get("specialist_enemy_priority"),
+	instance.MinionOutlineExtension[SPECIALIST_ENEMY] = {
+		priority = self:get(SPECIALIST_ENEMY .. "_priority"),
+		color = self:get_color_array(SPECIALIST_ENEMY),
 		material_layers = OUTLINE_MATERIAL_LAYERS,
-		color = get_color_array("specialist_enemy"),
 		visibility_check = visibility_check
 	}
 
-	instance.MinionOutlineExtension.elite_enemy = {
-		priority = mod:get("elite_enemy_priority"),
+	instance.MinionOutlineExtension[ELITE_ENEMY] = {
+		priority = self:get(ELITE_ENEMY .. "_priority"),
+		color = self:get_color_array(ELITE_ENEMY),
 		material_layers = OUTLINE_MATERIAL_LAYERS,
-		color = get_color_array("elite_enemy"),
 		visibility_check = visibility_check
 	}
 
-	instance.MinionOutlineExtension.monster_enemy = {
-		priority = mod:get("monster_enemy_priority"),
+	instance.MinionOutlineExtension[MONSTER_ENEMY] = {
+		priority = self:get(MONSTER_ENEMY .. "_priority"),
+		color = self:get_color_array(MONSTER_ENEMY),
 		material_layers = OUTLINE_MATERIAL_LAYERS,
-		color = get_color_array("monster_enemy"),
 		visibility_check = visibility_check
 	}
 end
 
-mod.on_setting_changed = function(setting_name)
+mod.on_setting_changed = function(setting_id)
 	mod:update_outline_settings(OutlineSettings)
 end
 
@@ -104,7 +109,19 @@ local function get_breed_tags(enemy_unit_data_extension)
 	return nil
 end
 
-function mod.tick()
+local function set_outline_color(unit, outline_unit_data_extension)
+	if not unit or not outline_unit_data_extension then return end
+
+	local top_outline = outline_unit_data_extension.outlines[1]
+	if top_outline then
+		local color = outline_unit_data_extension.settings[top_outline.name].color
+		if color then
+			Unit.set_vector3_for_materials(unit, "outline_color", Vector3(color[1], color[2], color[3]), true)
+		end
+	end
+end
+
+function mod.tick(self)
 	if not Managers.state or not Managers.state.extension then return end
 
 	local has_outline_system = Managers.state.extension:has_system("outline_system")
@@ -112,27 +129,30 @@ function mod.tick()
 		local enemy_units = get_enemy_units()
 		local outline_system = Managers.state.extension:system("outline_system")
 		for enemy_unit, _ in pairs(enemy_units) do
-			
 			local outline_unit_data_extension = outline_system._unit_extension_data[enemy_unit]
 			if outline_unit_data_extension and outline_unit_data_extension.outlines then
 				local outlines_count = #outline_unit_data_extension.outlines
 				if outlines_count > 1 then
-					outline_system:remove_outline(enemy_unit, "all_enemy")
-					outline_system:remove_outline(enemy_unit, "monster_enemy")
-					outline_system:remove_outline(enemy_unit, "specialist_enemy")
-					outline_system:remove_outline(enemy_unit, "elite_enemy")
+					outline_system:remove_outline(enemy_unit, ALL_ENEMY)
+					outline_system:remove_outline(enemy_unit, MONSTER_ENEMY)
+					outline_system:remove_outline(enemy_unit, SPECIALIST_ENEMY)
+					outline_system:remove_outline(enemy_unit, ELITE_ENEMY)
 				elseif outlines_count < 1 then
 					local enemy_unit_data_extension = ScriptUnit.has_extension(enemy_unit, "unit_data_system")
 					local breed_tags = get_breed_tags(enemy_unit_data_extension)
 					if breed_tags then
-						if mod:get(ELITE_ENEMY_SETTING) and (breed_tags.elite or breed_tags.captain) then
-							outline_system:add_outline(enemy_unit, "elite_enemy")
-						elseif mod:get(SPECIALIST_ENEMY_SETTING) and breed_tags.special then
-							outline_system:add_outline(enemy_unit, "specialist_enemy")
-						elseif mod:get(MONSTER_ENEMY_ENABLED_SETTING) and breed_tags.monster then
-							outline_system:add_outline(enemy_unit, "monster_enemy")
-						elseif mod:get(ALL_ENEMY_ENABLED_SETTING) then
-							outline_system:add_outline(enemy_unit, "all_enemy")
+						if self:get(ELITE_ENEMY_SETTING) and (breed_tags.elite or breed_tags.captain) then
+							outline_system:add_outline(enemy_unit, ELITE_ENEMY)
+							set_outline_color(enemy_unit, outline_unit_data_extension)
+						elseif self:get(SPECIALIST_ENEMY_SETTING) and breed_tags.special then
+							outline_system:add_outline(enemy_unit, SPECIALIST_ENEMY)
+							set_outline_color(enemy_unit, outline_unit_data_extension)
+						elseif self:get(MONSTER_ENEMY_ENABLED_SETTING) and breed_tags.monster then
+							outline_system:add_outline(enemy_unit, MONSTER_ENEMY)
+							set_outline_color(enemy_unit, outline_unit_data_extension)
+						elseif self:get(ALL_ENEMY_ENABLED_SETTING) then
+							outline_system:add_outline(enemy_unit, ALL_ENEMY)
+							set_outline_color(enemy_unit, outline_unit_data_extension)
 						end
 					end
 				end
@@ -148,7 +168,7 @@ function mod.update(dt)
 	else
 		cooldown = 0.3 -- 300ms
 		if mod:get(ENABLE_MOD_SETTING) then
-			mod.tick()
+			mod:tick()
 		end
 	end
 end
